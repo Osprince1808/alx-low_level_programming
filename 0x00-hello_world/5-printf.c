3cdae7f05cf245ee30ece0839f74b117c8b46d8b
Osprince1808 <osmanopk22@mail.com>
2022-04-08 14:35:18 -0700

    my 5th task commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,11 +2,11 @@
 /**
  *
  *main - Entry point
- *My hello, print task
- *Return: return 1 after program execution 
+ *
+ *Return: Always 0 (Success) 
  */ 
 int main(void)
 {
-printf ("with proper grammar. but the outcome is a piece of art. \n"):
-return (0):
+printf ("with proper grammar, but the outcome is a piece of art. \n"):
+  return (0);
 }
