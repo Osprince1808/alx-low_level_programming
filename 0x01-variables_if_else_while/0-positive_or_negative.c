c1ac1c8752ab142ced83b9a65899276d655e96dc
Osprince1808 <osmanopk22@mail.com>
2022-04-10 15:44:10 -0700

    My Zero task commit

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -17,11 +17,12 @@ n = rand() - RAND_MAX / 2;
 
 if (n==0)
 {
-printf("% i is zero\n", n);
+printf("%i is zero\n", n);
 }
 
 else if (n<0)
-{printf("%i is negative\n", n);
+{
+printf("%i is negative\n", n);
 }
 
 
@@ -33,3 +34,6 @@ printf("%i is positive\n", n);
 return (0);
 
 }
+
+
+
