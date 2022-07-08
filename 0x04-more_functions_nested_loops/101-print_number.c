0653c4fef0312bd307c7c58423a3a24d3350f058
Osprince1808 <osmanopk22@gmail.com>
2022-07-08 16:21:37 -0700

    101-print_number.c

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,28 +1,26 @@
 #include "main.h"
 #include <stdio.h>
 /**
-*print number  - prints int with putcher
-*@n: takes number  , hello
-*
-*Return: void
-*/
+ * print_number - prints int with putcher
+ * @n: takes number, hello
+ *
+ * Return: void
+ */
 
-void print_number(int  n)
+void print_number(int n)
 {
 unsigned int m;
 
-if (n  < 0)
+if (n < 0)
 {
-_putcher(‘-‘);
-M = n;
+_putcher('-');
+m = n;
 }
 else
 {
 m = n;
 }
 if (m / 10 != 0)
-Print number (m / 10);
-Putcher  ((m  % 10)  +  0’);
+print_number(m / 10);
+_putcher((m % 10) + '0');
 }
-
-
