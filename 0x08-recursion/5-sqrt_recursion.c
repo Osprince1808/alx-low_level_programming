e521f2bb6fcde279316479a0cbeb48df4e772c44
Osprince1808 <osmanopk22@gmail.com>
2022-07-20 17:46:48 -0700

    5-sqrt_recursion.c

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -16,13 +16,13 @@ int _sqrt_recursion(int n)
  */
 int halp(int c, int i)
 {
-int square;
+  int square;
 
-square = i * i;
-if (square == c)
-return (i);
-else if (square < c)
-return (halp(c, i + 1));
-else
-return (-1);
+  square = i * i;
+  if (square == c)
+    return (i);
+  else if (square < c)
+    return (halp(c, i + 1));
+  else
+    return (-1);
 }
