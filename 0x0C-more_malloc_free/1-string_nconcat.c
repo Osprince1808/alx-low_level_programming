90b8654a964bed6b50eaeabadf4469724150da9a
Osprince1808 <osmanopk22@gmail.com>
2022-08-04 14:08:43 -0700

    1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -1,44 +1,49 @@
 #include "main.h"
-#include <stdio.h>
 #include <stdlib.h>
-/**
- * _memset -  initializes n bytes of memory to x
- * @ptr: initial adress
- * @x: variable to initialize with
- * @n: number of bytes to initialize
- *
- * Return: Return pointer char (so movements are 1 byte)
- */
-char *_memset(char *ptr, int x, unsigned int n)
-{
-	unsigned int i;
-
-	for (i = 0; i < n; i++)
-		ptr[i] = x;
-
-	return (ptr);
-}
 
 /**
- * _calloc -  allocates memory using malloc and initializes in 0
- * @nmemb: number of elements of array to allocate
- * @size: size of elements
+ * string_nconcat - concatenates two strings.
+ * @s1: first string.
+ * @s2: second string.
+ * @n: amount of bytes.
  *
- * Return: Pointer to allocated memory or normal process termination
- * with a status value of 98
+ * Return: pointer to the allocated memory.
+ * if malloc fails, status value is equal to 98.
  */
-void *_calloc(unsigned int nmemb, unsigned int size)
+char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	void *p;
+	char *sout;
+	unsigned int ls1, ls2, lsout, i;
 
+	if (s1 == NULL)
+		s1 = "";
 
-	if (nmemb == 0 || size == 0)
-		return (0);
+	if (s2 == NULL)
+		s2 = "";
 
-	p = malloc(nmemb * size);
-	if (p == 0)
-		return (0);
-	_memset(p, 0, size * nmemb);
+	for (ls1 = 0; s1[ls1] != '\0'; ls1++)
+		;
 
-	return (p);
+	for (ls2 = 0; s2[ls2] != '\0'; ls2++)
+		;
+
+	if (n > ls2)
+		n = ls2;
+
+	lsout = ls1 + n;
+
+	sout = malloc(lsout + 1);
+
+	if (sout == NULL)
+		return (NULL);
+
+	for (i = 0; i < lsout; i++)
+		if (i < ls1)
+			sout[i] = s1[i];
+		else
+			sout[i] = s2[i - ls1];
+
+	sout[i] = '\0';
+
+	return (sout);
 }
